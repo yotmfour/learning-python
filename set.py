6a17e4708bb7cfcf03198d97282cead59e5c9dac
yose <yseasunr@gmail.com>
2016-12-15 16:53:29 -0500

    learned how to restrict lists

---
+++ set.py
@@ -0,0 +1,14 @@
+zoo = ('Kangaroo', 'Leopard', 'Moose')
+print('Tuple:', zoo, '\tLength:', len(zoo))
+
+bag = {'Red', 'Green', 'Blue'}
+bag.add('Yellow')
+print('\nSet:', bag, '\tLength', len(bag))
+print(type(bag))
+
+print( '\nIs Green In bag Set?:', 'Green' in bag)
+print('Is Orange In bag Set?:', 'Orange' in bag)
+
+box = {'Red', 'Purple', 'Yellow'}
+print('\nSet:', box, '\t\tLength', len(box))
+print('Common To Both Sets:', bag.intersection(box))
